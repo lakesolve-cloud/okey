117a0d0d573a872673222372920ca89b5b370192
harshleey <logun.fateem@gmail.com>
2026-06-06 20:05:27 +0100

    Fix: config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  nitro: {
+    preset: "vercel",
+    publicAssets: [
+      {
+        dir: "dist/client",
+        baseURL: "/",
+      },
+    ],
+  },
   tanstackStart: {
     server: { entry: "server" },
   },
